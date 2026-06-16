91d52461310a8ac46b13f848bdbe8c02ea60d0b8
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2026-06-16 13:14:59 +0300

    dd

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,10 @@
 // src/app/layout.jsx
+
 import "./globals.css";
 import { SITE } from "../lib/config.js";
 import HeaderClient from "../components/Header/Header.jsx";
 import "leaflet/dist/leaflet.css";
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL(`https://${SITE.domain}`),
@@ -21,7 +23,24 @@ export default function RootLayout({ children }) {
       <body>
         <HeaderClient />
         {children}
+
+        {/* Google Ads / Google Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17973794953"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-ads" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-17973794953');
+          `}
+        </Script>
       </body>
     </html>
   );
 }
+
